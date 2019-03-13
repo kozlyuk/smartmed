6d532dd32e100e4760a01bf8bdf448daf333b214
kozlyuk <sergey.kozlyuk@gmail.com>
2019-03-13 18:39:51 +0200

    Media folder removed.

--- smartmed/urls.py
+++ smartmed/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^django-webix/', include('django_webix.urls')),
 ]
 
 if settings.DEBUG:
