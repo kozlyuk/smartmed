f986c6986e6e0259c2d100fd2e8b8aa8c9b569c0
arrathilar <wervvolf@gmail.com>
2019-04-26 19:22:41 +0000

    add image crop form

--- catalogue/forms.py
+++ catalogue/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.forms import inlineformset_factory
 from catalogue.models import *
 from django.utils.translation import gettext_lazy as _
+# from PIL import Image
+from django.core.files import File
 
 
 class ProductFilterForm(forms.Form):
@@ -66,3 +68,31 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['name']
+        
+        
+        
+# test
+class ImageForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput())
+    y = forms.FloatField(widget=forms.HiddenInput())
+    width = forms.FloatField(widget=forms.HiddenInput())
+    height = forms.FloatField(widget=forms.HiddenInput())
+
+    # class Meta:
+    #     model = Product
+    #     fields = ('file', 'x', 'y', 'width', 'height', )
+
+    def save(self):
+        photo = super(PhotoForm, self).save()
+
+        x = self.cleaned_data.get('x')
+        y = self.cleaned_data.get('y')
+        w = self.cleaned_data.get('width')
+        h = self.cleaned_data.get('height')
+
+        image = Image.open(photo.file)
+        cropped_image = image.crop((x, y, w+x, h+y))
+        resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+        resized_image.save(photo.file.path)
+
+        return photo
