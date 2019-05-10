ad18ff23159f7cbfc79f9f0caa6951c939ebd05e
arrathilar <wervvolf@gmail.com>
2019-05-10 14:29:01 +0000

    add img_cutter plugin

---
+++ static/assets/plugins/img_cutter/img_cutter.js
@@ -0,0 +1,14 @@
+var resizeableImage = function(image_target) {
+    var $container,
+    orig_src = new Image(),
+    image_target = $(image_target).get(0),
+    event_state = {},
+    constrain = false,
+    min_width = 60,
+    min_height = 60,
+    max_width = 800,
+    max_height = 900,
+    resize_canvas = document.createElement('canvas');
+};
+
+resizeableImage($('.resize-image'));
