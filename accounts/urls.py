7f3bcee6153cd71c0df594173beb9475b341f214
kozlyuk <sergey.kozlyuk@gmail.com>
2019-05-12 15:40:17 +0300

    Auth urls fixed.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,39 +1,20 @@
 from django.urls import path
 
-from django.contrib.auth.views import(
-    LoginView,
-    LogoutView,
-#    PasswordResetView,
-#    PasswordResetDoneView,
-#    PasswordChangeView,
-#    PasswordChangeDoneView,
-#    PasswordResetConfirmView,
-#    PasswordResetCompleteView
-)
+from django.contrib.auth import views
+
 from accounts.views import *
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='auth.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='auth.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
 
-#    path('password_change/',
-#         PasswordChangeView.as_view(template_name='accounts/password_change_form.html'),
-#         name='password_change'),
-#    path('password_change/done/',
-#         PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
-#         name='password_change_done'),
-#    path('password_reset/',
-#         PasswordResetView.as_view(template_name='accounts/password_reset_form.html'),
-#         name='password_reset'),
-#    path('password_reset/done/',
-#         PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
-#         name='password_reset_done'),
-#    path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-#         PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
-#         name='password_reset_confirm'),
-#    path('reset/done/',
-#         PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
-#         name='password_reset_complete'),
+    path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('employee/list/', EmployeeList.as_view(), name='employee_list'),
     path('employee/create', EmployeeCreate.as_view(), name='employee_create'),
