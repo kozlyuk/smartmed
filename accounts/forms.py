2cbad87fdabdaeae5c11a604ba4414fc0d98d34e
kozlyuk <sergey.kozlyuk@gmail.com>
2019-05-20 11:58:24 +0300

    Imagecropping test. EmployeeSelfUpdateForm added.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import *
+from PIL import Image
 
 
 class EmployeeForm(forms.ModelForm):
@@ -9,9 +10,29 @@ class EmployeeForm(forms.ModelForm):
 
 
 class EmployeeSelfUpdateForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput())
+    y = forms.FloatField(widget=forms.HiddenInput())
+    width = forms.FloatField(widget=forms.HiddenInput())
+    height = forms.FloatField(widget=forms.HiddenInput())
+
     class Meta:
         model = Employee
-        fields = ['phone', 'avatar']
+        fields = ('phone', 'avatar', 'x', 'y', 'width', 'height', )
+
+    def save(self):
+        photo = super(EmployeeSelfUpdateForm, self).save()
+
+        x = self.cleaned_data.get('x')
+        y = self.cleaned_data.get('y')
+        w = self.cleaned_data.get('width')
+        h = self.cleaned_data.get('height')
+
+        image = Image.open(photo.file)
+        cropped_image = image.crop((x, y, w+x, h+y))
+        resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+        resized_image.save(photo.file.path)
+
+        return photo
 
 
 class PartnerForm(forms.ModelForm):
--- accounts/models.py
+++ accounts/models.py
@@ -11,8 +11,8 @@ THEME_CHOICES = (
 
 
 def avatar_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/avatars/<filename>
-    return 'avatars/{1}'.format(filename)
+    # file will be uploaded to MEDIA_ROOT/avatars/user_id/<filename>
+    return 'avatars/user_{0}/{1}'.format(instance.user.id, filename)
 
 
 class Employee(models.Model):
