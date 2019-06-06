a154ff594a83d12d9fb4be6a99553790962dd900
kozlyuk <sergey.kozlyuk@gmail.com>
2019-06-06 19:07:21 +0300

    employee_form saving fixed

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,29 +10,29 @@ class EmployeeForm(forms.ModelForm):
 
 
 class EmployeeSelfUpdateForm(forms.ModelForm):
-    x = forms.FloatField(widget=forms.HiddenInput())
-    y = forms.FloatField(widget=forms.HiddenInput())
-    width = forms.FloatField(widget=forms.HiddenInput())
-    height = forms.FloatField(widget=forms.HiddenInput())
+#    x = forms.FloatField(widget=forms.HiddenInput())
+#    y = forms.FloatField(widget=forms.HiddenInput())
+#    width = forms.FloatField(widget=forms.HiddenInput())
+#    height = forms.FloatField(widget=forms.HiddenInput())
 
     class Meta:
         model = Employee
-        fields = ('phone', 'avatar', 'x', 'y', 'width', 'height', )
+        fields = ('phone', 'avatar') #, 'x', 'y', 'width', 'height', )
 
-    def save(self):
-        photo = super(EmployeeSelfUpdateForm, self).save()
+#    def save(self):
+#        photo = super(EmployeeSelfUpdateForm, self).save()
 
-        x = self.cleaned_data.get('x')
-        y = self.cleaned_data.get('y')
-        w = self.cleaned_data.get('width')
-        h = self.cleaned_data.get('height')
+#        x = self.cleaned_data.get('x')
+#        y = self.cleaned_data.get('y')
+#        w = self.cleaned_data.get('width')
+#        h = self.cleaned_data.get('height')
 
-        image = Image.open(photo.file)
-        cropped_image = image.crop((x, y, w+x, h+y))
-        resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
-        resized_image.save(photo.file.path)
+#        image = Image.open(photo.file)
+#        cropped_image = image.crop((x, y, w+x, h+y))
+#        resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+#        resized_image.save(photo.file.path)
 
-        return photo
+#        return photo
 
 
 class PartnerForm(forms.ModelForm):
