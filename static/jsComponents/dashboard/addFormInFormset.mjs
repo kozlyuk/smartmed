4c8e7bb15751b9e383e98c34c5033f043b689831
Andrey <wervvolf@gmail.com>
2019-07-17 17:44:46 +0300

    refactoring (few)

--- static/jsComponents/dashboard/addFormInFormset.mjs
+++ static/jsComponents/dashboard/addFormInFormset.mjs
@@ -1,49 +1,53 @@
-function addFormInFormset () {
-    addNewRow();
+function addFormInFormset() {
+    $('.names-select2').select2();
+
+}
+
+function addNewRow() {
+  let targetFormRow = document.querySelector("#attributeList");
+  let clone = targetFormRow.cloneNode(true);
+
+  changeLastHiddenInputForDelete(clone);
+
+  document.getElementById("attributeListAll").appendChild(clone);
 };
 
-function addNewRow () {
-    let targetFormRow = document.querySelector('#attributeList');
-    let clone = targetFormRow.cloneNode(true);
-
-    changeLastHiddenInputForDelete(clone);
-
-    document.getElementById("attributeListAll").appendChild(clone);
-}
-
-function changeLastHiddenInputForDelete (element) {
-    //get ALL existing inputs
-    let hiddenInput = document.querySelectorAll("#deleteCheckboxes>input");
-    //get last input from existing inputs
-    let lastHiddenInput = hiddenInput[hiddenInput.length- 1]
-    //remove all symbol before number
-    let getAttribute = lastHiddenInput.getAttribute("name").replace(/attribute_set-/g, ' ');
+function cnageLastHiddenInputForSelect(element) {
     
-    //convert str to int
-    let inputId = parseInt(getAttribute, 10);  //get id from hidden input
-    let getParentDivCheckbox = element.lastElementChild;
-    let getCheckbox = getParentDivCheckbox.lastElementChild;
-    //create attribute
-    let attributeId = "id_attribute_set-" + (inputId +1) + "-DELETE";
-    let attributeName = "attribute_set-" + (inputId +1) + "-DELETE";
-
-    //remove id and name attributes
-    getCheckbox.removeAttribute("id");
-    getCheckbox.removeAttribute("name");
-    //put new attribute
-    getCheckbox.setAttribute("id", attributeId);
-    getCheckbox.setAttribute("name", attributeName);
 }
 
-function checkDeleteCheckboxes () {
+function changeLastHiddenInputForDelete(element) {
+  //get ALL existing inputs
+  let hiddenInput = document.querySelectorAll("#deleteCheckboxes>input");
+  //get last input from existing inputs
+  let lastHiddenInput = hiddenInput[hiddenInput.length - 1];
+  //remove all symbol before number
+  let getAttribute = lastHiddenInput
+    .getAttribute("name")
+    .replace(/attribute_set-/g, " ");
 
+  //convert str to int
+  let inputId = parseInt(getAttribute, 10); //get id from hidden input
+  let getParentDivCheckbox = element.lastElementChild;
+  let getCheckbox = getParentDivCheckbox.lastElementChild;
+  //create attribute
+  let attributeId = "id_attribute_set-" + (inputId + 1) + "-DELETE";
+  let attributeName = "attribute_set-" + (inputId + 1) + "-DELETE";
+
+  //remove id and name attributes
+  getCheckbox.removeAttribute("id");
+  getCheckbox.removeAttribute("name");
+  //put new attribute
+  getCheckbox.setAttribute("id", attributeId);
+  getCheckbox.setAttribute("name", attributeName);
 }
 
+function checkDeleteCheckboxes() {}
 
-export {addFormInFormset};
+export { addFormInFormset };
 
 // * * * * * * * * *
-//  jquery syntax 
+//  jquery syntax
 // * * * * * * * * *
 
 // $(function() {
